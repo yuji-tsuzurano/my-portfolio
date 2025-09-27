28e6c89f2e8b0089481cef1919d40ee3d1255128
TsuzuranoYuji <tsuzuranowork0119@gmail.com>
2025-09-27 15:23:20 +0900

    Updates

---
+++ _next/static/chunks/df4f6fcec031e57c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/9d27eb8be2dd4ccb.js",
+  "static/chunks/16b0dad1d524de78.js",
+  "static/chunks/turbopack-b397061c573faa9a.js"
+])
---
+++ _next/static/vm5ZGeFXjbPeOTcj1sgUh/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/64a1e6d6172d83ef.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/my-portfolio//_next/:path+",
+        "destination": "/my-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
