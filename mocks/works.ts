7f98ff8ad9ec1302aeb6d243e52060ac66ce18b4
TsuzuranoYuji <tsuzuranowork0119@gmail.com>
2025-09-29 17:44:30 +0900

    画像パスの修正

--- mocks/works.ts
+++ mocks/works.ts
@@ -8,7 +8,7 @@ export const works: WorkItem[] = [
   {
     title: "Portfolio（Yuji Tsuzuarno）",
     description: "Next.jsで開発したポートフォリオサイト",
-    image: "/portfolio.png",
+    image: "./portfolio.png",
     imageAlt: "Portfolio",
     href: "https://github.com/yuji-tsuzurano/my-portfolio",
     tags: ["Next.js", "TypeScript", "Tailwind CSS", "GitHub Pages"],
@@ -16,7 +16,7 @@ export const works: WorkItem[] = [
   {
     title: "SNS（Ibasaho）",
     description: "Blazor Serverで開発中の簡易的なSNSアプリ",
-    image: "/ibasho.png",
+    image: "./ibasho.png",
     imageAlt: "Ibasho",
     href: "https://github.com/yuji-tsuzurano/Ibasho",
     tags: ["Blazor", "C#", "Tailwind CSS", "PostgreSQL"],
